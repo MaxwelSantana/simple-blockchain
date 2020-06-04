d6a17c64c251df5dd78c35f4d4f92b9821dde304
MaxwelSantana <maxwel.santana@betternow.com.br>
2020-06-04 18:58:01 -0300

    ajust method that validade chain

--- src/main/java/block/BlockChain.java
+++ src/main/java/block/BlockChain.java
@@ -17,18 +17,24 @@ public class BlockChain {
 	public Boolean isChainValid() {
 		Block currentBlock; 
 		Block previousBlock;
+		String hashTarget = new String(new char[difficulty]).replace('\0', '0');
 		
-		for(int i=1;i < blockChain.size()-1;i++) {
+		for(int i=1;i < blockChain.size();i++) {
 			currentBlock = blockChain.get(i);
 			previousBlock = blockChain.get(i-1);
-			
+			//compare registered hash and calculated hash:
 			if(!currentBlock.hash.equals(currentBlock.calculateHash())) {
 				System.out.println("Current Hashes not equal");			
 				return false;
 			}
-			
+			//compare previous hash and registered previous hash
 			if(!previousBlock.hash.equals(currentBlock.previousHash)) {
 				System.out.println("Previous Hashes not equal");
+				return false;
+			}
+			//check if hash is solved
+			if(!currentBlock.hash.substring(0, difficulty).equals(hashTarget)) {
+				System.out.println("This block hasn't been mined");
 				return false;
 			}
 		}
